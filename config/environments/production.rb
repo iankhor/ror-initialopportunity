eecd60a8c9334adae73ffd0d4fcbdf0fea866b93
iankhor <iankhorbc.thecoder@gmail.com>
2017-02-01 12:18:46 +1100

    initial files with partial sendgrid details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: ENV[SENDGRID_USER_NAME],
+    password: ENV[SENDGRID_API_KEY]
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
