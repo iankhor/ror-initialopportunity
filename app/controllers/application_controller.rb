5243c5637e1e80f9a7922e49c3e005c047a68181
iankhor <iankhorbc.thecoder@gmail.com>
2017-02-06 14:04:57 +1100

    updated application controller to permit values

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, , :last_name, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number])
   end
 
   def after_sign_in_path_for(resource)
